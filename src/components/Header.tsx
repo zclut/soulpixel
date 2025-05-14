2b98f05e3cabed0046564407ebf3762d80b21cfc
Juan Daniel Padilla <juand.padilla@xaldigital.com>
2025-05-14 15:21:48 +0100

    protected route

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import { SignedIn, UserButton } from "@clerk/astro/react";
+
 const Header = () => {
   return (
     <header className="w-full py-4 px-6 flex justify-center items-center bg-gray-900/60 z-10">
-      ECHO
+      <SignedIn>
+        <UserButton showName={true} />
+      </SignedIn>
     </header>
   );
 };
